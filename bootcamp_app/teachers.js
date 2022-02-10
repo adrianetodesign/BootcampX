df025bbf718ce6c3c3087d67d58cfb02958d488e
adrianetodesign <adrianetodesign@gmail.com>
2022-02-10 03:02:14 +0000

    Add parameterized queries to sql calls

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,15 +6,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName || 'JUL02'}`];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`;
+
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
